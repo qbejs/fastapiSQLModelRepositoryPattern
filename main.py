d0ef290897e127691273f49867586ef4665129e0
Jakub Skowron <jakub@skowron.dev>
2024-01-02 23:25:51 +0100

    jwt auth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 from fastapi import FastAPI, Depends
+from fastapi.openapi.utils import get_openapi
 
 from Service.UserService import create_user
 from Utils.auth.utils import create_access_token, get_current_user
@@ -16,3 +17,28 @@ def root():
 @app.get("/token")
 async def say_hello(current_user: dict = Depends(get_current_user)):
     return {"message": "Protected endpoint", "logged_user": current_user}
+
+
+# Custom OpenAPI
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="FastAPI Boilerplate",
+        version="1.0.0",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {"url": "URL_do_loga"}
+    openapi_schema["components"]["securitySchemes"] = {
+        "bearerAuth": {"type": "http", "scheme": "bearer", "bearerFormat": "JWT"}
+    }
+
+    for path in openapi_schema["paths"].values():
+        for method in path.values():
+            method["security"] = [{"bearerAuth": []}]
+
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
